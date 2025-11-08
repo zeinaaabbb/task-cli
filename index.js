25128583ebc7799b51a238a74e9a52c9129e8420
zeinab <zeinab.warsama@ft.com>
2025-11-08 16:36:18 +0000

    feat: implement working add and list commands using fs operations

--- index.js
+++ index.js
@@ -1,46 +1,81 @@
-import tasksJson from './tasks.json' assert { type: 'json' };
 import process from 'process';
+import fs from 'fs'
 
 function list() {
-    tasksJson.tasks.forEach(task => {
-        console.log([task.id], task.description, "--", task.status)
-    })
+        try {
+            const data = fs.readFileSync('tasks.json', 'utf8');
+            const tasks = JSON.parse(data);
+
+            tasks.forEach(task => {
+                console.log([task.id], task.description, "--", task.status);
+            })
+
+            if (tasks.length === 0){
+                console.log("There are no tasks available to list. Use 'add' followed by your task description to create a new task entry!");
+            }
+
+        } catch (err) {
+            console.error(err);
+        }
 }
 
-list();
-
-// get the description text. If empty show error DONE
-// find the tasks.json DONE
-// if task.json doesnt exisit, be an empty list of task 
-// if task.json exisits read it DONE
-// if there is no task use 1 
-// otherwise finding the biggest exisiting ID and add 1
-// get the current time 
-// use it for the createAT and updateAt 
 
 function add(description) {
     if (!description || !description.trim()){
-        console.log("Please enter a description!");
-        process.exit(1)
+        console.log("To add a task, please enter a description!");
+        process.exit(1);
     }
 
-    if (tasksJson){
-        const tasks = tasksJson.tasks
+    const data = fs.readFileSync('tasks.json', 'utf8');
+    const tasks = JSON.parse(data);
 
-        const largestId = Math.max(...tasks.map(t => t.id))
-        console.log(largestId)
+    if (tasks){
+        try {
 
-        tasks.forEach(task => {
-            const taskId = task.id
-            if (!taskId) {
-                console.log("No tasks exisit!")
-                const taskId = 1
-                console.log("test:", taskId, description)
+        let createdAt = new Date().toISOString();
+        const updatedAt = new Date().toISOString();
+
+        if (tasks.length === 0){
+
+            const newTask = {
+                "id": 1,
+                "description": description,
+                "status": "todo",
+                "createdAt": createdAt,
+                "updatedAt": updatedAt
             }
-        })   
+
+
+            tasks.push(newTask);
+            fs.writeFileSync('./tasks.json', JSON.stringify(tasks, null, 2));
+            console.log("updated successfully:", newTask);
+
+        }
+        
+        if (tasks.length > 0) {
+            const largestId = Math.max(...tasks?.map(t => t.id))
+
+            const addNewId = largestId + 1
+            const newTask = { 
+                "id": addNewId,
+                "description": description,
+                "status": "todo",
+                "createdAt": createdAt,
+                "updatedAt": updatedAt
+            }
+
+            tasks.push(newTask);
+            fs.writeFileSync('./tasks.json', JSON.stringify(tasks, null, 2));
+            console.log("updated successfully:", newTask);
+        }
+
+        } catch (err) {
+            console.log(err);
+        }
 
     }
 }
+
 
 const argumentList = process.argv.slice(2)
 const command = argumentList[0]
@@ -49,13 +84,10 @@ const description = argumentList.slice(1).join(" ")
 if (command === "add"){
     add(description);
 }
-// get all the words the user typed in the terminal after node index.js
-// the first word after the index.js is the command 
-// Everuthing after the command is joined together into one string
-// the command is is add 
-// call the add() function and give it the description from the string above(" ")
 
-add()
+if (command === "list"){
+    list();
+}
 
 
 
