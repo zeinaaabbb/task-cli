281532a86eab8e3a4b880be715bf9a5c0e2b3419
zeinab <zeinab.warsama@ft.com>
2025-11-11 23:27:50 +0000

    feat: implement update task functionality

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ function list() {
                 console.log("There are no tasks available to list. Use 'add' followed by your task description to create a new task entry!");
             }
 
-        } catch (err) {
-            console.error(err);
+        } catch (error) {
+            console.error(error);
         }
 }
 
@@ -48,7 +48,7 @@ function add(description) {
 
             tasks.push(newTask);
             fs.writeFileSync('./tasks.json', JSON.stringify(tasks, null, 2));
-            console.log("updated successfully:", newTask);
+            console.log("Task added successfully:", newTask);
 
         }
         
@@ -66,28 +66,75 @@ function add(description) {
 
             tasks.push(newTask);
             fs.writeFileSync('./tasks.json', JSON.stringify(tasks, null, 2));
-            console.log("updated successfully:", newTask);
+            console.log("Task added successfully:", newTask);
         }
 
-        } catch (err) {
-            console.log(err);
+        } catch (error) {
+            console.log(error);
         }
 
     }
 }
 
+function update({id, description}) {
+    if (!id) {
+        console.log("\nProvide the 'id' of the task you would like to update followed by the new task 'description'\n")
+        return list()
+    }
+
+    if (!description) {
+        console.log("\nProvide the 'description' of the new task\n")
+        return list()
+    }
+
+    try {
+        const data = fs.readFileSync('tasks.json', 'utf8');
+        const tasks = JSON.parse(data);
+
+        const index = tasks.findIndex((t => t.id === id))
+
+        if (index === -1 ){
+            console.log("This task id has not been found")
+            return list();
+        }
+        
+        const updatedTask = {
+            id: tasks[index].id,
+            description: description,
+            status: tasks[index].status,
+            createdAt: tasks[index].createdAt,
+            updatedAt: new Date().toISOString()
+        }
+        
+        tasks[index] = updatedTask
+        
+        fs.writeFileSync('./tasks.json', JSON.stringify(tasks, null, 2));
+        console.log("Task updated successfully:", updatedTask);
+
+    } catch(error) {
+        console.error(error)
+    }
+
+}
+
+function deleteTask() {
+    console.log("I want to make a deletion")
+}
+
 
 const argumentList = process.argv.slice(2)
 const command = argumentList[0]
-const description = argumentList.slice(1).join(" ")
+const id = Number(argumentList[1])
+const description = argumentList.slice(2).join(" ")
 
 if (command === "add"){
     add(description);
-}
-
-if (command === "list"){
+} else if (command === "list"){
     list();
+} else if (command === "update"){
+    update({ id,  description });
+} else if (command === "delete") {
+    deleteTask();
+} else {
+    console.log(" :) Your Task Manager:",`\n`, `\n`,"Please select the following commands:",`\n`,"-'list' ",`\n`,"-'add' ", `\n`,"-'update' ", `\n`,"-'delete'" )
 }
-
-
-
